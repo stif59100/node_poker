022758e4e76ef1ba3638a27ceef5514c66a8184b
sandydelhoute <sdelhoute@gmail.com>
2021-12-23 13:14:47 +0100

    update sql file

--- sql/scripts_sql/foreign_key.sql
+++ sql/scripts_sql/foreign_key.sql
@@ -2,6 +2,6 @@ ALTER TABLE rights_users ADD FOREIGN KEY (id_user) REFERENCES user(id_user);
 ALTER TABLE rights_users ADD FOREIGN KEY (id_right) REFERENCES rights(id_right);
 ALTER TABLE round_player ADD FOREIGN KEY (id_player) REFERENCES user(id_user);
 ALTER TABLE round_player ADD FOREIGN KEY (id_round) REFERENCES round(id_round);
-ALTER TABLE level_round ADD FOREIGN KEY (id_level) REFERENCES level(id_level);
+ALTER TABLE level_round ADD FOREIGN KEY (id_level) REFERENCES `level`(id_level);
 ALTER TABLE level_round ADD FOREIGN KEY (id_round) REFERENCES round(id_round);
 ALTER TABLE token ADD FOREIGN KEY (id_user) REFERENCES user(id_user);
